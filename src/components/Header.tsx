cab501f66766d68ab587f28687df32d21c154380
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2023-04-25 22:07:20 -0300

    feat: remove route Home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,14 +14,13 @@ const Header = () => {
     <header className="flex justify-center items-center px-4 py-2 bg-gray-100 w-full">
       <div className="flex justify-between items-center w-full max-w-6xl">
         <div className="flex items-center p-4">
-          <Image src={logo} alt="Logo" width={225} />
+          <a href="/">
+            <Image src={logo} alt="Logo" width={225} />
+          </a>
         </div>
 
         <nav className='hidden lg:block'>
           <ul className="flex">
-            <li className="mx-2">
-              <a href="/">Home</a>
-            </li>
             <li className="mx-2">
               <a href="/graduacao">Graduação</a>
             </li>
@@ -55,9 +54,6 @@ const Header = () => {
           className={`${isMenuOpen ? 'flex' : 'hidden'} bg-gray-300 flex-1 p-2 gap-1 lg:hidden absolute top-24 right-0 h-[calc(100%-6rem)] w-full justify-center`}
         >
           <ul className="flex flex-col text-center">
-            <li className="mx-2 p-2 hover:underline">
-              <a href="/">Home</a>
-            </li>
             <li className="mx-2 p-2 hover:underline">
               <a href="/graduacao">Graduação</a>
             </li>
