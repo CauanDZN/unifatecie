ac511e38e4d8e75230c328b1cc963f30de8d1f45
Cauan Victor <cauanvictor0325@gmail.com>
2023-04-27 15:19:23 -0300

    feat: adding blockquote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Header from '@/components/Header'
 import matriculas from '../../public/matriculas-abertas.png'
 import celular from '../../public/unifatecie-celular.png'
+import { KnowMore } from '@/components/KnowMore'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,6 +14,17 @@ export default function Home() {
       <div className='items-center justify-center flex flex-wrap bg-orange-600 w-full p-4'>
         <Image src={matriculas} alt='Foto da matrícula' />
         <Image src={celular} alt='Foto da matrícula' />
+      </div>
+      <div className='items-center justify-center flex flex-wrap bg-white w-full p-4'>
+        <KnowMore />
+        
+      </div>
+      <div>
+          <button title="Saiba Mais" />
+          <button title="Graduação" />
+          <button title="Pós" />
+          <button title="Matricule-se" />
+          <button title="Contatos" />
       </div>
     </>
   )
---
+++ src/components/KnowMore.tsx
@@ -0,0 +1,23 @@
+export function KnowMore(){
+    return (
+        <figure className="bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+            <img className="w-24 h-24 rounded-full mx-auto" src="https://static.dw.com/image/680783_804.jpg" alt="" width="384" height="512" />
+            <div className="pt-6 space-y-4">
+                <blockquote>
+                <p className="text-lg font-medium">
+                    “A educação é o nosso passaporte para o futuro, 
+                    pois o amanhã pertence às pessoas que se preparam hoje.”
+                </p>
+                </blockquote>
+                <figcaption className="font-medium">
+                <div className="text-sky-500 dark:text-sky-400">
+                    Malcolm X
+                </div>
+                <div className="text-slate-700 dark:text-slate-500">
+                    Ativista dos Direitos Humanos | Nebraska - EUA
+                </div>
+                </figcaption>
+            </div>
+        </figure>
+    )
+}
